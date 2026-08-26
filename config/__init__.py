d4ac747eb485c1334061fa59f8a83e1a02492239
Twoh11 <101970565+Twoh11@users.noreply.github.com>
2026-08-26 23:46:28 +0800

    Delete config directory

--- config/__init__.py
+++
@@ -1,8 +0,0 @@
-import yaml
-from easydict import EasyDict
-
-with open("config/train.yaml", "r") as f:
-    training_config = EasyDict(yaml.load(f, Loader=yaml.SafeLoader))
-
-with open("config/vis.yaml", "r") as f:
-    vis_config = EasyDict(yaml.load(f, Loader=yaml.SafeLoader))
--- config/seed.py
+++
@@ -1,8 +0,0 @@
-import torch
-import numpy as np
-import random
-
-SEED = 42
-torch.manual_seed(SEED)
-random.seed(SEED)
-np.random.seed(SEED)
